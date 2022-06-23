d5fe1075b9b529d25128884351db4505b3fe9257
Selena <selen15@yandex.ru>
2022-06-24 00:14:30 +0500

    HW.Task_1 max and min number

---
+++ HW.Task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+//какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.WriteLine("Hello, Amigo!");
+Console.WriteLine("Введите любое целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a!=b)
+{
+    if (a > b)
+    {
+        Console.WriteLine("ваше первое число {0} является наибольшим", a);
+        Console.WriteLine("ваше второе число {0} является наименьшим", b);
+    }
+
+    else
+    {
+        Console.WriteLine("ваше второе число {0} является наибольшим", b);
+        Console.WriteLine("ваше первое число {0} является наименьшим", a);
+    }
+}
+else
+{
+    Console.WriteLine("ваши числа равны");
+}
