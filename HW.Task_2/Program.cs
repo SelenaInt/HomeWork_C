41dbe2303e86804306b5de564893bda2b7513a1e
Selena <selen15@yandex.ru>
2022-06-24 03:08:18 +0500

    HW.Task_2 max number of three

---
+++ HW.Task_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.WriteLine("Hello, Amigo");
+Console.WriteLine("Введите любое целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+if (a > b && a > c)
+    {
+        Console.WriteLine("максимальное из трех чисел: {0} ", a);
+    }
+    else if (b > a && b > c)
+    {
+        Console.WriteLine("максимальное из трех чисел: {0} ", b);
+    }
+    else
+    {
+        Console.WriteLine("максимальное из трех чисел: {0} ", c);
+    }
+
+
+
+
+
+
