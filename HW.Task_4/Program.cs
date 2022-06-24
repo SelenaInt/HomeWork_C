636b8668359d00cdb97ea49dfc877b1393101837
Selena <selen15@yandex.ru>
2022-06-24 20:08:40 +0500

    HW.Task_4 even range numbers_changes

--- HW.Task_4/Program.cs
+++ HW.Task_4/Program.cs
@@ -4,17 +4,29 @@ Console.WriteLine("Hello, Amigo!");
 
 Console.Write("Введите любое целое число : ");
 int N = Convert.ToInt32(Console.ReadLine());
-
 int i = 1;
-
-Console.WriteLine("Наши четные числа диапазона от 1 до введенного вами  " +N);
+// Console.WriteLine("Наши четные числа диапазона от 1 до введенного вами  " +N);
 
 while  (i <= N) 
-{
-    if (i % 2 == 0) // (i % 2 == 0) сортировка четного числа
 
     {
-        Console.WriteLine(i);
+        if (i == 2)
+        {
+           Console.WriteLine("Наши четные числа диапазона от 1 до введенного вами  " +N); 
+        }
+        if (i % 2 == 0) // (i % 2 == 0) сортировка четного числа
+
+        {
+            Console.WriteLine(i);
+            
+        }
+        i +=1;
+        
+        if (N <= 1)
+        {
+          Console.WriteLine("нет четных чисел в данном диапазоне");  
+        }
     }
-    i +=1;
-}
+
+
+ 
